a02b48d0979a00b08bcafb95656197ebe7b8d289
Dan Cherouny <dcherouny@gmail.com>
2015-12-03 16:55:03 -0500

    Add precompile to application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module Sinterest
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.browserify_rails.commandline_options = "-t reactify --extension=\".js.jsx\""
+    config.assets.initialize_on_precompile = false
   end
 end
